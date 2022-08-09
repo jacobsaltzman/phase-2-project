42fc61ce6706537440b251713861fc77880c7643
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2022-08-08 20:28:04 -0700

    Adds Header component and moves Darkmode button component to it

---
+++ phase-2-project/src/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Darkmode from "./Darkmode";
+
+const Header = () => {
+  return(
+    <header className="App-header">
+      <Darkmode />
+
+      <h1>
+        This is Sparta!
+      </h1>
+    </header>
+  )
+}
+
+export default Header;
+
