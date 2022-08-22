75e6fdbc9ae2dfed24b9b0e2871cb42c0716cea0
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2022-08-22 12:54:28 -0700

    moves state and effect variables from Main Page up to App

--- phase-2-project/src/App.js
+++ phase-2-project/src/App.js
@@ -1,14 +1,26 @@
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 import MainPage from './components/MainPage.js';
 import NewCoinForm from "./components/NewCoinForm.js";
 
 function App() {
+
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/coins")
+      .then((r) => r.json())
+      .then((data)=> setCoins(data))
+  }, [])
+
+  console.log(coins)
+
+
   return (
     <div className="App">
       <Header />
-      <MainPage />
+      <MainPage coins={coins}/>
       <NewCoinForm />
     </div>
   );
--- phase-2-project/src/components/MainPage.js
+++ phase-2-project/src/components/MainPage.js
@@ -1,17 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import CoinContainer from "./CoinContainer.js";
 
-function MainPage(){
-
-  const [coins, setCoins] = useState([]);
-
-  useEffect(() => {
-    fetch("http://localhost:3001/coins")
-      .then((r) => r.json())
-      .then((data)=> setCoins(data))
-  }, [])
-
-  console.log(coins)
+function MainPage({ coins }){
   
   return(
     <div>
