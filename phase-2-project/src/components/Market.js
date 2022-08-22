607d6630ac7bb66bc504a09dafdefb606bfd9d48
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2022-08-22 13:39:37 -0700

    Builds the foundation for a Market and Portfolio component

---
+++ phase-2-project/src/components/Market.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Market(){
+  return(
+    <div>
+    <h1>Market Pairs</h1>
+    <p>This is where the api pricing data will go!</p>
+    </div>
+  )
+}
+
+export default Market;
---
+++ phase-2-project/src/components/Portfolio.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Portfolio(){
+  return(
+    <div>
+    <h1>My Portfolio</h1>
+    <p>This is where the Portfolio will go!</p>
+    </div>
+  )
+}
+
+export default Portfolio;
