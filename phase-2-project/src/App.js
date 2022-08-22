b647c4acfa2e2c507065fbdf439752105d49ce5e
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2022-08-21 19:27:21 -0700

    Adds a main page and builds a fetch request for db.json

--- phase-2-project/src/App.js
+++ phase-2-project/src/App.js
@@ -1,10 +1,12 @@
-import './App.css';
+
 import Header from './components/Header';
+import MainPage from './components/MainPage.js';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MainPage />
     </div>
   );
 }
---
+++ phase-2-project/src/components/MainPage.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react";
+
+function MainPage(){
+  const [coins, setCoins] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:3001/coins")
+      .then((r) => r.json())
+      .then(setCoins)
+      console.log(coins)
+      
+  }, [])
+
+
+  return(
+    <div>
+   
+    </div>
+  )
+
+}
+
+export default MainPage;
