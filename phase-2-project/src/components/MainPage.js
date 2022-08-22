9fd82c606fe8c0567c15ee5f2bbba12c73725f52
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2022-08-22 02:16:35 -0700

    Gets the db.json connected with the main page fetch request.

--- phase-2-project/src/components/MainPage.js
+++ phase-2-project/src/components/MainPage.js
@@ -1,17 +1,17 @@
 import React, { useEffect, useState } from "react";
 
 function MainPage(){
+
   const [coins, setCoins] = useState([]);
 
   useEffect(() => {
     fetch("http://localhost:3001/coins")
       .then((r) => r.json())
-      .then(setCoins)
-      console.log(coins)
-      
+      .then((data)=> setCoins(data))
   }, [])
 
-
+  console.log(coins)
+  
   return(
     <div>
       <h1>Top Coins</h1>
