caffe0780c6fbf0530e42fd47aa50611b6c8488f
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2022-08-21 22:36:51 -0700

    Builds the new coin form out more and adds README information

--- phase-2-project/src/App.js
+++ phase-2-project/src/App.js
@@ -1,12 +1,14 @@
 
 import Header from './components/Header';
 import MainPage from './components/MainPage.js';
+import NewCoinForm from "./components/NewCoinForm.js";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <MainPage />
+      <NewCoinForm />
     </div>
   );
 }
--- phase-2-project/src/components/NewCoinForm.js
+++ phase-2-project/src/components/NewCoinForm.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const NewCoinForm(){
+function NewCoinForm(){
 
   return (
     <section>
@@ -8,19 +8,30 @@ const NewCoinForm(){
         <h3>Add New Coin</h3>
 
         <label>Name</label>
+        <input type="text" id="name" name="name"/>
 
         <label>Ticker Symbol</label>
+        <input type="text" id="ticker" name="ticker"/>
 
         <label>Consensus Mechanism</label>
+        <select name="type" id="type">
+          <option>Proof of Work</option>
+          <option>Proof of Stake</option>
+          <option>Stablecoin</option>
+          <option>Other</option>
+        </select>
 
         <label>Current Price</label>
+        <input type="number" id="price" name="price"/>
 
-        <label>Basecode Language</label>
+        <label>Language of Base Code</label>
+        <input type="text" id="language" name="language"/>
 
+        <button type="submit">Add Coin</button>
       </form>
     </section>
   )
 
-
-
 }
+
+export default NewCoinForm;
