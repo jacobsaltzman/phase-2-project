7c55f69eab47586d4f0cb44bf7effe17edc4c2d2
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2022-08-24 11:56:57 -0700

    Fixes embed gif class with className

--- phase-2-project/src/components/Portfolio.js
+++ phase-2-project/src/components/Portfolio.js
@@ -5,7 +5,7 @@ function Portfolio(){
     <div>
     <h1>My Portfolio</h1>
     <p>Coming Soon!</p>
-    <iframe src="https://giphy.com/embed/VekcnHOwOI5So" title="cat-gif" width="480" height="240" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+    <iframe src="https://giphy.com/embed/VekcnHOwOI5So" title="cat-gif" width="480" height="240" frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
     </div>
   )
 }
