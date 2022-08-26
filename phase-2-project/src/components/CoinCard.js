700cee9792be7e7705344e93f319312cbb5d00e7
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2022-08-26 01:09:23 -0700

    adds link elements to card for github and white paper data

--- phase-2-project/src/components/CoinCard.js
+++ phase-2-project/src/components/CoinCard.js
@@ -2,17 +2,17 @@ import React from "react";
 
 function CoinCard({coin}){
 
-  const { id, name, ticker, price, type, language, image } = coin;
+  const { id, name, ticker, price, type, language, image, wp, github } = coin;
 
   return(
     <ul className="card" id={id}>
       <h3>{name}</h3>
       <iframe src={image} className="giphy-embed" allowFullScreen title={name}></iframe>
-      <h4>{ticker} @ ${price}</h4>
+      <h4>{ticker} @ ${price}</h4> <button id="plus-button">Add to Portfolio</button>
       <p>{name} is a {type} blockchain, originally coded in {language}</p>
       <br/>
-      <button id="plus-button">Add to Portfolio</button>
-      <button id="learn-button">Learn more</button>
+      <a href={github}>GitHub</a>
+      <a href={wp}>White Paper</a>
     </ul>
   )
 }
